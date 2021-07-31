202b75ed795e708485f4078e7d74540a9081519f
Jin Dou <SamwiseDou@outlook.com>
2021-07-30 21:10:57 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+  name="mTRFpy",
+  version="0.0.1",
+  author="Powerfulbean",
+  author_email="powerfulbean@gmail.com",
+  long_description_content_type="text/markdown",
+  url="https://github.com/powerfulbean/mTRFpy",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
