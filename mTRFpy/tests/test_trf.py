7eea5d268896a17f15dbed2b01c06aa3f85fbfc4
ole <ole.bialas@posteo.de>
2022-05-19 14:11:07 +0000

    finish unittest for arithmetic operations on TRF class

--- mTRFpy/tests/test_trf.py
+++ mTRFpy/tests/test_trf.py
@@ -14,3 +14,8 @@ def test_arithmatic():
     trf3 = trf1+trf2
     np.testing.assert_equal(trf3.b, trf1.b+trf2.b)
     np.testing.assert_equal(trf3.w, trf1.w+trf2.w)
+    trf4 = sum([trf1, trf2, trf3])
+    np.testing.assert_equal(trf4.b, trf1.b+trf2.b+trf3.b)
+    np.testing.assert_equal(trf4.w, trf1.w+trf2.w+trf3.w)
+    trf4 /= 4
+    np.testing.assert_equal(trf4.w, trf1.w)
