21ade2d0cb022dcb64f1654038722411037df2bd
ole <ole.bialas@posteo.de>
2022-06-10 15:25:54 -0400

    add packward model to testing

--- mTRFpy/tests/test_model.py
+++ mTRFpy/tests/test_model.py
@@ -5,23 +5,30 @@ from mTRFpy.Model import TRF, cross_validate
 root = Path(__file__).parent.absolute()
 
 
-def test_train():
+def test_train_forward():
     speech_response = loadmat(str(root/'data'/'speech_data.mat'))
     fs = speech_response['fs'][0][0]
-    response = speech_response['resp']
-    stimulus = speech_response['stim']
+    response = speech_response['resp'][0:100]
+    stimulus = speech_response['stim'][0:100]
     for i in range(5):
         reps = np.random.randint(2, 10)
         stimuli = np.stack([stimulus for _ in range(reps)])
         responses = np.stack([response for _ in range(reps)])
         tmin = np.random.uniform(-0.1, 0.05)
         tmax = np.random.uniform(0.1, 0.4)
+        direction = np.random.choice([1, -1])
         regularization = np.random.uniform(0, 10)
-        trf1 = TRF()
+        trf1 = TRF(direction=direction)
         trf1.train(stimuli, responses, fs, tmin, tmax, regularization)
-        trf2 = TRF()
+        trf2 = TRF(direction=direction)
         trf2.train(stimuli[0], responses[0], fs, tmin, tmax, regularization)
-        np.testing.assert_almost_equal(trf1.weights, trf2.weights, 11)
+        if direction == 1:
+            assert trf1.weights.shape[0] == stimuli.shape[-1]
+            assert trf1.weights.shape[-1] == response.shape[-1]
+        if direction == -1:
+            assert trf1.weights.shape[0] == stimuli.shape[-1]
+            assert trf1.weights.shape[-1] == response.shape[-1]
+        np.testing.assert_almost_equal(trf1.weights, trf2.weights, 10)
 
 
 def test_predict():
