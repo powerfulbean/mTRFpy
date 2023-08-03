0f4c6045d6c9579bc2ae83c3cb0935c413b35d12
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2023-08-03 14:52:59 +0200

    use viewcode extension

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,7 @@ extensions = [
     "sphinx.ext.duration",
     "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
