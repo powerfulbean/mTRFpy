5afc271bdd544f0ed02eafebceb5bccd5c8b2684
obi <ole.bialas@posteo.de>
2026-03-21 20:53:26 +0100

    don't modify the data length

--- mtrf/stats.py
+++ mtrf/stats.py
@@ -444,8 +444,8 @@ def permutation_distribution(
         xp.random.seed(seed)
     x, y, tmin, tmax = _get_xy(stimulus, response, tmin, tmax, model.direction)
     min_len = min([len(x_i) for x_i in x])
-    for i, x_i in enumerate(x):
-        x[i], y[i] = x_i[:min_len], y[i][:min_len]
+    x = [x_i[:min_len] for x_i in x]
+    y = [y_i[:min_len] for y_i in y]
     k = _check_k(k, n_trials)
     idx = xp.arange(n_trials)
     combinations = xp.transpose(xp.meshgrid(idx, idx)).reshape(-1, 2)
